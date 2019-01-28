8d079769814bb2cb0870ee86577c643b185831b0
darkhorsecmd <1368306623@qq.com>
2019-01-28 23:07:35 +0800

    html 特殊字符 比如 & 在html里面是 &amp; 这里做了去除处理

--- test/gooseeker.py
+++ test/gooseeker.py
@@ -17,35 +17,41 @@ from lxml import etree
 class GsExtractor(object):
     def _init_(self):
         self.xslt = ""
+
     # 从文件读取xslt
-    def setXsltFromFile(self , xsltFilePath):
-        file = open(xsltFilePath , 'r' , encoding='UTF-8')
+    def setXsltFromFile(self, xsltFilePath):
+        file = open(xsltFilePath, 'r', encoding='UTF-8')
         try:
             self.xslt = file.read()
         finally:
             file.close()
+
     # 从字符串获得xslt
-    def setXsltFromMem(self , xsltStr):
+    def setXsltFromMem(self, xsltStr):
         self.xslt = xsltStr
+
     # 通过GooSeeker API接口获得xslt
-    def setXsltFromAPI(self , APIKey , theme, middle=None, bname=None):
-        apiurl = "http://www.gooseeker.com/api/getextractor?key="+ APIKey +"&theme="+quote(theme)
+    def setXsltFromAPI(self, APIKey, theme, middle=None, bname=None):
+        apiurl = "http://www.gooseeker.com/api/getextractor?key=" + APIKey + "&theme=" + quote(theme)
         if (middle):
-            apiurl = apiurl + "&middle="+quote(middle)
+            apiurl = apiurl + "&middle=" + quote(middle)
         if (bname):
-            apiurl = apiurl + "&bname="+quote(bname)
+            apiurl = apiurl + "&bname=" + quote(bname)
         apiconn = request.urlopen(apiurl)
         self.xslt = apiconn.read()
+
     # 返回当前xslt
     def getXslt(self):
         return self.xslt
+
     # 提取方法，入参是一个HTML DOM对象，返回是提取结果
-    def extract(self , html):
+    def extract(self, html):
         xslt_root = etree.XML(self.xslt)
         transform = etree.XSLT(xslt_root)
         result_tree = transform(html)
         return result_tree
+
     # 提取方法，入参是html源码，返回是提取结果
-    def extractHTML(self , html):
+    def extractHTML(self, html):
         doc = etree.HTML(html)
         return self.extract(doc)
--- test/readPdf.py
+++ test/readPdf.py
@@ -5,18 +5,18 @@
 from urllib import request
 from lxml import etree
 from gooseeker import GsExtractor
+import html
 # 访问并读取网页内容
-url = "http://cs.njust.edu.cn/1735/list1.htm"
+url = "http://im.nju.edu.cn/teachers.do?type=1&mid=4"
 conn = request.urlopen(url)
 doc = etree.HTML(conn.read())
 
 bbsExtra = GsExtractor()    # 生成xsltExtractor对象
-bbsExtra.setXsltFromAPI("e346796c93c6ba7441636666e401e5cc", "hehai_test")
+bbsExtra.setXsltFromAPI("e346796c93c6ba7441636666e401e5cc", "im.nju.edu.cn")
 xs=bbsExtra.getXslt()
-print(str(xs))
-pass
-result = bbsExtra.extract(doc)    # 调用extract方法提取所需内容
+result = html.unescape(str(bbsExtra.extract(doc)))    # 调用extract方法提取所需内容
+# result = bbsExtra.extract(doc)
 # out file
 file_name = 'E:/parse_detail_'+'.xml'
-open(file_name, "wb").write(result)
-print(str(result))
+open(file_name, "w").write(result)
+# print(str(result))
